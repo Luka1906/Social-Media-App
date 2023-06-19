a92e5aab424b58b99a12095e692829536515c129
Luka Matovic <lukamatovic1994@gmail.com>
2023-06-18 23:00:13 -0500

    Creating post routes,model and controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const {User} = require("../models/User");
-const genAuthToken = require("../util/AuthToken");
+const genAuthToken = require("../utils/AuthToken");
 
 
 /* REGISTER USER */
