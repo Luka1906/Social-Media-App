90bb8346e69a467aa2eb7d5b26b4afb2cfc1574b
Luka Matovic <lukamatovic1994@gmail.com>
2023-07-09 03:58:23 -0500

    fixing post route

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picture", image);
       formData.append("picturePath", image.name);
     }
-    const response = await fetch("https://social-media-7sek.onrender.com/posts", {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
